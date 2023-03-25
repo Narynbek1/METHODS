dd42826e3218226b1e4e02d561f2e04280087124
naryn <narynbeknurbekov@gmail.com>
2023-03-26 00:22:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        method();
+//
+//        int[] mas2 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        for (int i = 0; i < mas2.length; i++) {
+//            System.out.println(mas2[i]+" ");
+//        }
+//        hello();
+//    }
+//
+//    private static void hello() {
+//    }
+//
+//    static void  method (){
+//        System.out.println("hello");
+
+
+     method();
+
+    }
+
+    static void method() {
+        Scanner s = new Scanner(System.in);
+        int year = s.nextInt();
+        int age = 2023-year;
+        System.out.println(age);
+
+
+
+
+    }
+}
